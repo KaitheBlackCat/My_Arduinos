a8c65e2763de1654f8fd5072cfc5d70ad58a8708
KaitheBlackCat <ingabbang@gmail.com>
2022-06-12 14:54:24 +0900

    I2C Scanner Code To find address

---
+++ i2c_Scanner/i2c_Scanner.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+#include "DM_G_I2C.h"
+
+void setup()
+{
+  Wire.begin();         // I2C 통신
+  Serial.begin(9600);  // 시리얼 모니터를 통해 확인
+  while (!Serial);     // 시리얼 모니터 동작 했을 때 까지 기다리기
+   
+  G_I2C_Scanner();      // I2C 주소 확인 하기 
+}
+
+
+void loop()
+{
+    
+}
