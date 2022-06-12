71ed3fb1daaff4a7924898efbfa13cbfa5c1c6ca
KaitheBlackCat <98504189+KaitheBlackCat@users.noreply.github.com>
2022-06-12 15:11:03 +0900

    Add files via upload

---
+++ voltage/voltage.ino
@@ -0,0 +1,19 @@
+float vout = 0.0;
+float vin = 0.0;
+float R1 = 30000.0;
+float R2 = 7500.0;
+int SPIN = A1;
+int value = 0;
+void setup(){
+pinMode(SPIN, INPUT);
+Serial.begin(9600);
+Serial.print("Voltage:");
+}
+void loop(){
+value = analogRead(SPIN);
+vout = (value * 5.0) / 1024.0;
+vin = vout / ( R2 / ( R1 + R2) );
+Serial.print("V: ");
+Serial.println(vin,2);
+delay(100);
+}
